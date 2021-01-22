3e111f9fe483038b3619cd6b2b3ce6e452805e04
vchacin <arq.vchacin@gmail.com>
2021-01-22 13:35:40 -0300

    initial files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const boton1 = document.querySelector("#miBoton");
+// Agregar listener
+boton1.addEventListener("click", function(evento){
+	// Aquí todo el código que se ejecuta cuando se da click al botón
+	alert("Le has dado click Buscar");
+});
+
+const boton2 = document.querySelector("#suerte");
+// Agregar listener
+boton2.addEventListener("click", function(evento){
+	// Aquí todo el código que se ejecuta cuando se da click al botón
+	alert("Le has dado click Suerte");
+});
